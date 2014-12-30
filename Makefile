c86a34271f3e4de452d2a5750c852b8eaa0cee5d
mvcrouse <mvcrouse@indiana.edu>
2014-12-30 17:39:52 -0500

    made code functional on mac

--- Makefile
+++ Makefile
@@ -1,19 +1,25 @@
-OPT= -O -m32
+OPT= -O 
+#-m32
 
 all: main32
 
 main32: main32.o multiply.o myutil.o
-	gcc $(OPT) -o main32 main32.o multiply.o myutil.o -lgmp -lrt
+	gcc-4.8 $(OPT) -o main32 main32.o multiply.o myutil.o -lgmp
+# -lrt
 
 main32.o: main32.c multiply.h
-	gcc $(OPT) -c main32.c
+	gcc-4.8 $(OPT) -c main32.c
 
 multiply.o: multiply.c multiply.h
-	gcc $(OPT) -c multiply.c
+	gcc-4.8 $(OPT) -c multiply.c
 
 myutil.o: myutil.c
-	gcc $(OPT) -c myutil.c
+	gcc-4.8 $(OPT) -c myutil.c
 
 clean: 
+	make -i clear
+
+clear:
+	rm *~
 	rm *.o main32
 
--- myutil.c
+++ myutil.c
@@ -1,13 +1,30 @@
+#include <mach/mach_time.h>
 #include <time.h>
 #include <gmp.h>
+#include <stdio.h>
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+
+#ifdef __MACH__
+#include <sys/time.h>
+//clock_gettime is not implemented on OSX
+int clock_gettime(struct timespec* t) {
+  struct timeval now;
+  int rv = gettimeofday(&now, NULL);
+  if (rv) return rv;
+  t->tv_sec  = now.tv_sec;
+  t->tv_nsec = now.tv_usec * 1000;
+  return 0;
+}
+#endif
 
 double mytime (void)
 {
   int flag;
-  clockid_t cid = CLOCK_REALTIME; // CLOCK_MONOTONE might be better
+  
   struct timespec tp;
   double timing;
-  flag = clock_gettime(cid, &tp);
+  flag = clock_gettime(&tp);
     if (flag == 0) timing = tp.tv_sec + 1.0e-9*tp.tv_nsec;
     else           timing = -17.0;
 
